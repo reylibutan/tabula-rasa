813bd3235838cbfd13d40fad6783f05da46be890
Rey Libutan <reylibutan@gmail.com>
2018-07-15 03:10:34 +0800

    Initial Gradle commit

---
+++ src/main/java/com/reylibutan/tabularasa/TabularasaApplication.java
@@ -0,0 +1,12 @@
+package com.reylibutan.tabularasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabularasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabularasaApplication.class, args);
+	}
+}
